39433a9e5470437aa3c6a65049415d1c7266cfd3
Karina Lucas <karina.lucas@gmail.com>
2026-05-06 12:15:12 -0300

    feat: logout

--- src/app/features/admin/dashboard/sidebar/sidebar.ts
+++ src/app/features/admin/dashboard/sidebar/sidebar.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatAnchor, MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faUsers, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../../../../services/auth-service';
 
 @Component({
   selector: 'app-sidebar',
@@ -12,6 +13,12 @@ import { faUsers, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-ic
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  private auth = inject(AuthService);
+
   faUsers = faUsers;
   faLogout = faArrowRightFromBracket;
+
+  protected handleLogout() {
+    this.auth.logout();
+  }
 }
